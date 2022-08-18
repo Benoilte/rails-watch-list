478f9932d6a5d9e84f7fbd81ab10c92411b58544
benoit brandt <benoit.07@hotmail.com>
2022-08-18 20:52:20 +0200

    updating CSS

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
 
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
