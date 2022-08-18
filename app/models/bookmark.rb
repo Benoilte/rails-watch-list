1c8eaaf22576fa8e6771fb795691fbd0b5278126
benoit brandt <benoit.07@hotmail.com>
2022-08-18 12:45:26 +0200

    create validation for models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, :list, presence: true
+  validates :movie, uniqueness: { scope: :list, message: "this movie-list combination already exist" }
+
+  validates :comment, length: { minimum: 6 }
 end
