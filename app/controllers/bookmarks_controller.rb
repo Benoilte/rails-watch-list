472dc79325949e48d151813a0790573ded0c2925
benoit brandt <benoit.07@hotmail.com>
2022-08-21 21:21:38 +0200

    improving style to my card and show list of films

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,23 +5,9 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new
   end
 
-  # <select class="form-select is-invalid select required" aria-invalid="true"
-  # name="bookmark[movie_id]" id="bookmark_movie_id"><option value="" label=" "></option>
-  # <option value="1">Whiplash</option>
-  # <option value="2">The Grapes of Wrath</option>
-  # <option value="3">Sunset Blvd.</option>
-  # <option value="4">The Sting</option>
-  # <option value="5">Toy Story 3</option>
-  # <option value="6">Goodfellas</option>
-  # <option value="7">WALL·E</option>
-  # <option value="8">Titanic</option>
-  # <option value="9">Ocean's Eight</option></select>
-
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    # @bookmark.comment = params[:bookmark][:comment]
-    # @bookmark.movie_id = params[:bookmark][:movie_id]
     if @bookmark.save
       redirect_to list_path(@list)
     else
